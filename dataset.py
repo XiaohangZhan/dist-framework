1bad88e9425b79ce0358ba890a5290653717ee7c
XiaohangZhan <xiaohangzhan@outlook.com>
2019-08-16 21:24:40 +0800

    update

--- dataset.py
+++ dataset.py
@@ -6,6 +6,7 @@ import sys
 from PIL import Image
 
 import torch
+import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset
 
@@ -14,7 +15,7 @@ import utils # some data preprocessing funcs in utils/data_utils.py
 
 # implement your dataset here
 
-class ImageDataset(Dataset):
+class ImageLabelDataset(Dataset):
 
     def __init__(self, config, phase):
         '''
@@ -40,7 +41,7 @@ class ImageDataset(Dataset):
 
     def __getitem__(self, idx):
         img_fn = self.fns[idx]
-        label = self.lablels[idx]
+        label = self.labels[idx]
         image = np.array(Image.open(img_fn).convert('RGB'))
 
         # resize w.r.t. short size or long size
@@ -66,4 +67,53 @@ class ImageDataset(Dataset):
         image = torch.from_numpy(image.astype(np.float32)).transpose((2, 0, 1)) # 3HW
         image = self.transforms(image)
         return image, label
-        image
+
+class ImageDataset(Dataset):
+
+    def __init__(self, config):
+        '''
+        config: data config
+        phase: train or val
+        '''
+        self.img_transform = transforms.Compose([
+            transforms.Normalize(config['data_mean'], config['data_div'])
+        ])
+        with open(config['{}_list.txt'], 'r') as f:
+            lines = f.readlines()
+        self.fns = [os.path.join(config['{}_root'], l.strip()) for l in lines]
+        self.crop_size = config['crop_size']
+        self.short_size = config.get('short_size', None)
+        self.long_size = config.get('long_size', None)
+
+    def __len__(self):
+        return len(self.fns)
+
+    def __getitem__(self, idx):
+        img_fn = self.fns[idx]
+        image = np.array(Image.open(img_fn).convert('RGB'))
+
+        # resize w.r.t. short size or long size
+        if self.short_size is not None or self.long_size is not None:
+            image = utils.image_resize(
+                image, short_size=self.short_size, long_size=self.long_size)
+
+        # crop
+        centerx = image.shape[1] // 2
+        centery = image.shape[0] // 2
+        sizex, sizey = self.crop_size
+        bbox = [centerx - sizex / 2, centery - sizey / 2, sizex, sizey]
+        image = cv2.resize(utils.crop_padding(image, bbox, pad_value=(0, 0, 0)),
+                           self.crop_size, interpolation=cv2.INTER_CUBIC)
+
+        # transform
+        image = torch.from_numpy(image.astype(np.float32)).transpose((2, 0, 1)) # 3HW
+        image = self.transforms(image)
+        return image
+
+
+def Cifar10(config, phase):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(config['data_mean'], config['data_div'])])
+    return torchvision.datasets.CIFAR10(root='./data', train=phase=='train',
+                                        download=True, transform=transform)
--- trainer.py
+++ trainer.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 
 import models
 import utils
-from dataset import ImageDataset
+import dataset
 import pdb
 
 class Trainer(object):
@@ -84,9 +84,11 @@ class Trainer(object):
         self.curr_step = self.start_iter
 
         # lr scheduler & datasets
-        trainval_class = ImageDataset
-        eval_class = ImageDataset
-        extract_class = ImageDataset
+        trainval_class = dataset.__dict__[args.data['trainval_dataset']]
+        eval_class = (None if args.data['eval_dataset']
+                      is None else dataset.__dict__[args.data['eval_dataset']])
+        extract_class = (None if args.data['extract_dataset']
+                         is None else dataset.__dict__[args.data['extract_dataset']])
 
         if not (args.validate or args.extract or args.evaluate):  # train
             self.lr_scheduler = utils.StepLRScheduler(
